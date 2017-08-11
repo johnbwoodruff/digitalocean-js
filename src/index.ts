ed3bdf95279f82f4dedd3c03fc4fbdb96a626571
John Woodruff <johnwoodruff91@gmail.com>
2017-08-10 23:27:15 -0600

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@
 export * from './lib/services/account/account-service';
 export * from './lib/services/actions/actions-service';
 export * from './lib/services/block-storage/block-storage-service';
+export * from './lib/services/block-storage-actions/block-storage-actions-service';
+export * from './lib/services/certificate/certificate-service';
+export * from './lib/services/domain/domain-service';
 
 // Models
 export * from './lib/models/account';
 export * from './lib/models/action';
 export * from './lib/models/block-storage';
+export * from './lib/models/certificate';
+export * from './lib/models/domain';
 export * from './lib/models/region';
 export * from './lib/models/snapshot';
